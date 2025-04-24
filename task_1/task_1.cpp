1bc5bca751e7d3c38cbe340937c5603023b7d1ad
bloodfallen <74541116+CHIKENlvl19@users.noreply.github.com>
2025-04-24 23:52:59 +0700

    destroyed task 1

---
+++ task_1/task_1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void line(){
+    cout << "---------------------" << endl;
+}
+
+void value_print(float x, float y){
+    cout << "|" << setw(5) << fixed << setprecision(1) << x << setw(5) << "|" << fixed << setprecision(2) << y << setw(5) << "|" << endl;
+}
+
+int main() {
+    float x = -6;
+    float y = 0;
+
+    cout << "Таблица значений функции:" << endl;
+    line();
+    cout << "|" << setw(5) << "x" << setw(5) << "|" << setw(5) << "y" << setw(5) << "|" << endl;
+    line();
+
+    for(x; x <= -2; x++){
+        y = -0.992 * pow(( x + 3.5), 2) + 2.2;
+        value_print(x, y);
+    }
+
+    for(x; x <= 2; x++){
+        y = -sqrt(4 - x*x);
+        value_print(x, y);
+    }
+
+    for(x; x <= 8; x++){
+        y = ( log(x) / log(2) ) - 1;
+        value_print(x, y);
+    }
+
+    for(x; x <= 10; x++){
+        y = -x + 9;
+        value_print(x, y);
+    }
+
+
+    return 0;
+}
